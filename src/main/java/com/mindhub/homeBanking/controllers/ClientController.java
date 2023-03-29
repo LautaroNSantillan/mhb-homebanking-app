d7bc70c622b8615d6d8ca031382ef6b902de75cd
SLautaro <111198390+LautaroNSantillan@users.noreply.github.com>
2023-03-29 18:42:07 -0300

    quick fixes for register and loans transacion

--- src/main/java/com/mindhub/homeBanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/homeBanking/controllers/ClientController.java
@@ -3,24 +3,33 @@ package com.mindhub.homeBanking.controllers;
 import com.mindhub.homeBanking.dtos.AccountDTO;
 import com.mindhub.homeBanking.dtos.CardDTO;
 import com.mindhub.homeBanking.dtos.ClientDTO;
+import com.mindhub.homeBanking.models.Account;
 import com.mindhub.homeBanking.models.Client;
 import com.mindhub.homeBanking.models.Loan;
+import com.mindhub.homeBanking.repositories.ClientRepository;
 import com.mindhub.homeBanking.services.AccountService;
 import com.mindhub.homeBanking.services.ClientService;
 import com.mindhub.homeBanking.services.impl.AccountServiceImpl;
 import com.mindhub.homeBanking.services.impl.ClientServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 @CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("/api")
 public class ClientController {
+    @Autowired
+    ClientRepository clientRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     private final ClientService clientService;
     private final AccountService accService;
@@ -74,13 +83,22 @@ public class ClientController {
             return new ResponseEntity<>("Email already in use", HttpStatus.FORBIDDEN);
         }
 
-       clientService.saveNewClient(incomingObj.getFirstName(), incomingObj.getLastName(), incomingObj.getEmail(), incomingObj.getPassword());
+        Client client = new Client(incomingObj.getFirstName(), incomingObj.getLastName(), incomingObj.getEmail(), passwordEncoder.encode(incomingObj.getPassword()));
+
+        clientRepository.save(client);
+
+      // clientService.saveNewClient(incomingObj.getFirstName(), incomingObj.getLastName(), incomingObj.getEmail(), incomingObj.getPassword());
         return new ResponseEntity<>("Logged in",HttpStatus.CREATED);
     }
 
     @GetMapping("clients/current")
     public ClientDTO getCurrentClient(Authentication authentication) {
-        return new ClientDTO(clientService.findClientByEmailExcludingDisabledAccounts(authentication.getName()));
+        System.out.println(authentication.getName());
+        Client client = clientService.findByEmail(authentication.getName());
+        Set<Account> notDisabledAccs =  client.getAccounts().stream().filter(account -> !account.isDisabled()).collect(Collectors.toSet());
+        client.setAccounts(notDisabledAccs);
+        return new ClientDTO(client);
+
     }
     @GetMapping("clients/current/cards")
     public List<CardDTO> getCurrentCards(Authentication authentication) {
